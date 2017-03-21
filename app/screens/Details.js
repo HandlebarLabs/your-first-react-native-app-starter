080c2e60b92c47116e52821b458127386c3b5264
Spencer Carli <spencercarli@gmail.com>
2017-03-21 11:23:41 -0500

    Use scrollview

--- app/screens/Details.js
+++ app/screens/Details.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Text, ScrollView } from 'react-native';
 
 import { Header } from '../components/UserDetails';
 import colors from '../config/colors';
@@ -9,9 +9,9 @@ class Details extends Component {
     const contact = this.props.navigation.state.params;
 
     return (
-      <View style={{ backgroundColor: colors.background }}>
+      <ScrollView style={{ backgroundColor: colors.background }}>
         <Header {...contact} />
-      </View>
+      </ScrollView>
     );
   }
 }
