69db0e30618728724f8d1dc95b9b7125a4604d81
Spencer Carli <spencercarli@gmail.com>
2017-03-23 11:27:45 -0500

    Field progression

--- app/components/TextInput/TextInput.js
+++ app/components/TextInput/TextInput.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, TextInput } from 'react-native';
 
 import styles from './styles';
 
-const CustomTextInput = (props) => {
-  return (
-    <View style={styles.container}>
-      <TextInput
-        autoCorrect={false}
-        autoCapitalize="none"
-        style={styles.input}
-        {...props}
-      />
-    </View>
-  );
+class CustomTextInput extends Component {
+  focus = () => {
+    this._input.focus();
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          autoCorrect={false}
+          autoCapitalize="none"
+          style={styles.input}
+          ref={(input) => this._input = input}
+          {...this.props}
+        />
+      </View>
+    );
+  }
 };
 
 export default CustomTextInput;
--- app/screens/NewContact.js
+++ app/screens/NewContact.js
@@ -31,18 +31,26 @@ class NewContact extends Component {
     this.setState(mod);
   }
 
-  handleSubmit = () => {
-    alert('Submit');
+  handleSubmit = (index, override = false) => {
+    if (index === fields.length - 1 || override) {
+      alert('Submit');
+    } else {
+      const nextField = fields[index + 1];
+      this[nextField.stateKey].focus();
+    }
   };
 
   render() {
     return (
       <KeyboardAwareScrollView style={{ backgroundColor: colors.background }}>
         {
-          fields.map((field) => (
+          fields.map((field, index) => (
             <TextInput
               key={field.stateKey}
               onChange={(text) => this.onInputChange(text, field.stateKey)}
+              returnKeyType={index === fields.length - 1 ? 'done' : 'next'}
+              onSubmitEditing={() => this.handleSubmit(index)}
+              ref={(input) => this[field.stateKey] = input}
               {...field}
             />
           ))
@@ -50,7 +58,7 @@ class NewContact extends Component {
         <View style={{ marginTop: 20 }}>
           <PrimaryButton
             label="Save"
-            onPress={() => this.handleSubmit()}
+            onPress={() => this.handleSubmit(0, true)}
           />
         </View>
       </KeyboardAwareScrollView>
